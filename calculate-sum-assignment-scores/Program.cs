d3e572db79c8c3b46f297aec92723243d58a7890
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2023-09-03 13:08:00 -0600

    added the functions for averaging scores and returning a letter grade, while displaying those to console

--- calculate-sum-assignment-scores/Program.cs
+++ calculate-sum-assignment-scores/Program.cs
@@ -1,4 +1,24 @@
-﻿Func<int, int, int, int, int, int> calcSum = (a, b, c, d, e) => a + b + c + d + e;
+﻿// functions to get summation of scores, average of scores, and letter grade
+Func<int, int, int, int, int, int> calcSum = (a, b, c, d, e) => a + b + c + d + e;
+Func<decimal, int, decimal> averageScore = (a, b) => a / b;
+Func<decimal, string> getLetterGrade = finalScore => 
+{
+    switch (finalScore)
+        {
+            case decimal n when n >= 97:
+                return "A+";
+            case decimal n when n <= 96 && n >= 93:
+                return "A";
+            case decimal n when n <= 92 && n >= 90:
+                return "A-";
+            case decimal n when n <= 89 && n >= 87:
+                return "B+";
+            case decimal n when n <= 86 && n >= 83:
+                return "B";
+            default:
+                return "No score";
+        };
+};
 
 // initialize variables - graded assignments 
 int currentAssignments = 5;
@@ -27,14 +47,33 @@ int jeong3 = 98;
 int jeong4 = 100;
 int jeong5 = 97;
 
+// set decimals for average scores
+decimal sophiaScore;
+decimal nicolasScore;
+decimal zahirahScore;
+decimal jeongScore;
+
 // initial variables for students set to sum using calcSum() function
-int sophiaSum = calcSum(sophia1, sophia2, sophia3, sophia4, sophia5);
-int nicolasSum = calcSum(nicolas1, nicolas2, nicolas3, nicolas4, nicolas5);
-int zahirahSum = calcSum(zahirah1, zahirah2, zahirah3, zahirah4, zahirah5);
-int jeongSum = calcSum(jeong1, jeong2, jeong3, jeong4, jeong5);
+decimal sophiaSum = calcSum(sophia1, sophia2, sophia3, sophia4, sophia5);
+decimal nicolasSum = calcSum(nicolas1, nicolas2, nicolas3, nicolas4, nicolas5);
+decimal zahirahSum = calcSum(zahirah1, zahirah2, zahirah3, zahirah4, zahirah5);
+decimal jeongSum = calcSum(jeong1, jeong2, jeong3, jeong4, jeong5);
+
+// calculate average scores using the averageScore() function
+sophiaScore = averageScore(sophiaSum, currentAssignments);
+nicolasScore = averageScore(nicolasSum, currentAssignments);
+zahirahScore = averageScore(zahirahSum, currentAssignments);
+jeongScore = averageScore(jeongSum, currentAssignments);
 
 // display the student name and the value of summed assignment scores
-Console.WriteLine("Sophia: " + sophiaSum);
-Console.WriteLine("Nicolas: " + nicolasSum);
-Console.WriteLine("Zahirah: " + zahirahSum);
-Console.WriteLine("Jeong: " + jeongSum);
+// commented out, as not needed in the final showing
+// Console.WriteLine("Sophia: " + sophiaSum);
+// Console.WriteLine("Nicolas: " + nicolasSum);
+// Console.WriteLine("Zahirah: " + zahirahSum);
+// Console.WriteLine("Jeong: " + jeongSum);
+
+// write out the average scores
+Console.WriteLine($"Sophia: {sophiaScore} {getLetterGrade(sophiaScore)}");
+Console.WriteLine($"Nicolas: {nicolasScore} {getLetterGrade(nicolasScore)}");
+Console.WriteLine($"Zahirah: {zahirahScore} {getLetterGrade(zahirahScore)}");
+Console.WriteLine($"Jeong: {jeongScore} {getLetterGrade(jeongScore)}");
